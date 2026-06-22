1f68bd8ac002ec3c3a170939c5055a25b8048807
Anil <anilkumarmaridi6@gmail.com>
2026-06-22 11:20:53 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Banking_adv_v18';
+}
