a36244c32abe393ab369c551faaaab9d834fe28c
Anil <anilkumarmaridi6@gmail.com>
2026-06-24 22:04:13 +0530

    feat(step-9): implement global loader system with signal-based request tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NotificationContainerComponent } from './shared/toast/components/notification-container/notification-container.component';
+import { LoaderComponent } from './shared/ui/loader/loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NotificationContainerComponent],
-  template: ` <app-notification-container></app-notification-container>
+  imports: [RouterOutlet, NotificationContainerComponent, LoaderComponent],
+  template: ` <app-loader></app-loader>
+    <app-notification-container></app-notification-container>
     <router-outlet></router-outlet>`,
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,3 +16,4 @@ import { NotificationContainerComponent } from './shared/toast/components/notifi
 export class AppComponent {
   title = 'Banking_adv_v18';
 }
+
